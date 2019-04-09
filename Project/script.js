cf739befac575216575e871e9aca44d339174572
Dmitry <chuperka@yandex.ru>
2019-04-09 17:52:13 +0300

    Это мой первый коммит

---
+++ Project/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+a = 5;
+console.log(a);
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let something;
+
+console.log(second);
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ["plum.png","orange.jpg","apple.png"];
+
+console.log (arr[2]);
+
+/* alert("Hello, world!"); */
+
+/* let answer = confirm ("Are you here?");
+
+console.log(answer); */
+
+/* let answer = +prompt ("Есть ли вам 18 лет?","Да");
+
+console.log(typeof(answer)); */
+
+let incr = 10,
+    decr = 10;
+ 
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" ===2);
+
+let isCheked = false;
+let isClose = false;
+
+console.log(isCheked || !isClose);
+
---
+++ Project/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let arr = ["plum.png","orange.jpg","apple.png"];
+
+console.log (arr[2]);
